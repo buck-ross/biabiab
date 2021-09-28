1fe89fa7750d56f561d6f4423b32e4ee9ab800bf
Buckley Ross <brr322@lehigh.edu>
2021-09-28 12:34:11 -0400

    fix(nonce): hash concatenation as a string
    In addition to fixing the concatenation issue, I also modified the main
    application files to include the nonce computation algorithm & use that
    after constructing a merkle tree.
    
    Signed-off-by: Buckley Ross <brr322@lehigh.edu>
    Change-Id: I5e161d1d9450cc9740cca879c22526782323a349

--- code/main.js
+++ code/main.js
@@ -7,9 +7,13 @@ window.addEventListener('load', async function() {
 	// Construct the merkel tree:
 	const tree = await mktree(accounts);
 
+	// Calculate the nonce:
+	const nonce_target = Math.pow(2, 255);
+	const nonce = await calculate_nonce(tree[0][0], nonce_target);
+
 	// Return the results to the test harness:
 	const res = document.createElement('p');
 	res.id = 'result';
-	res.innerText = tree[0][0];
+	res.innerText = tree[0][0] + ' ' + nonce;
 	document.body.appendChild(res);
 });
--- code/nonce.js
+++ code/nonce.js
@@ -1,34 +1,35 @@
 async function try_nonce(nonce, root_hash, target){
-    // concatenate
-    // hash
-    let nonce_hash = await hash(root_hash + i); // TODO concat properly
+	// Compute the hash of the root hash concatenated with the nonce:
+	const nonce_hash = stringify_hash(await hash_string(root_hash + nonce));
 
-    // test against target
-    if (nonce_hash <= target) {
-        return nonce;
-    }
+	// test against target
+	if (parseInt(nonce_hash, 16) <= target) {
+		return nonce;
+	}
 
-    return Promise.reject();
+	return Promise.reject();
 }
 
-/* calculate nonce
- * @param root_hash 
- * @param target
- * @return correct nonce */
+/**
+* calculate nonce
+* @param root_hash
+* @param target
+* @return correct nonce
+*/
 async function calculate_nonce(root_hash, target) {
-    let nonce = null;
-    let a = 0;
-    let b = 5; // batch size
+	let nonce = null;
+	let a = 0;
+	let b = 5; // batch size
 
-    while (nonce == null) {
-        let tmp = [];
-        for (let i = a; i < a+b; i++) {
-            tmp.push(try_nonce(i, root_hash, target));
-        }
+	while (nonce == null) {
+		let tmp = [];
+		for (let i = a; i < a+b; i++) {
+			tmp.push(try_nonce(i, root_hash, target));
+		}
 
-        nonce = await Promise.any(tmp).catch((reason)=>null);
-        a += b;
-    }
-   
-    return nonce;
+		nonce = await Promise.any(tmp).catch((reason)=>null);
+		a += b;
+	}
+
+	return nonce;
 }
