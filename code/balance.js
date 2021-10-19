224e9d8f9efc32993bc22ad81abdd803c663f7fb
yiqun12 <71995731+yiqun12@users.noreply.github.com>
2021-10-18 22:39:01 -0400

    Update balance.js

--- code/balance.js
+++ code/balance.js
@@ -1,5 +1,8 @@
-function balance(array){//text should be an array like this 
-    //var array = text.split(/\r?\n/)//we aim for the address and balance which looks like 071acd5321b712745db188369d63619c8a654ef5208fe6a5c408f200ca69bd71 3945441296
+function balance(input,array){//input is the address and the array is the block
+  var flag = false;
+    //var array = text.split(/\r?\n/)
+  var input_ = new RegExp(input);
+
   for(let i = array.length-1; i>=0;i--){//search from new to old. 
   if (array[i].match(input_)) {
 //split array by space
